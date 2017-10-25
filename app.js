41af339c421cc17299e014af00cf529cf5edc3bd
stowcoal <cdstochl@gmail.com>
2017-10-25 15:02:13 -0500

    enable cors

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
